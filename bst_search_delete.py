8d9f07d6edc1d28bf41a4ad048feea19ac06c017
Khushboo Rawat <khushboorawat@109>
2026-03-10 19:25:58 +0530

    binary search tree insertion deletion searching

---
+++ bst_search_delete.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self,value):
+        self.left=None
+        self.right=None
+        self.data=value
+def insert(root,value):
+    if(root==None):
+        return Node(value)
+    if(root.data==value):
+        return root
+    if(root.data>value):
+        root.left=insert(root.left,value)
+    else:
+        root.right=insert(root.right,value)
+    return root
+
+def Search(root,value):
+    if(root==None):
+        print("Element not found",end=" \n")
+        return
+    if(root.data==value):
+        print("Element Found")
+        return 
+    if(root.data>value):
+        Search(root.left,value)
+    else:
+        Search(root.right,value)
+
+def get_successor(root):
+    root=root.right
+    while(root!=None and root.left!= None):
+        root=root.left
+    return root    
+
+
+def delete(root,value):
+    if(root==None):
+        return root
+    if(root.data>value):
+        delete(root.left,value)
+    elif(root.data<value):
+        root.right=delete(root.right,value)
+    else:
+        if(root.left==None):
+            return root.right
+        if(root.right==None):
+            return root.left
+        else:
+            succ = get_successor(root)
+            root.data=succ.data
+            root.right=delete(root.right,succ.data)
+        return root                        
+
+def Inorder(root):
+    if(root!=None):
+        Inorder(root.left)
+        print(root.data,end=" ")
+        Inorder(root.right)
+
+root= insert(None,20)
+root=insert(root,15)
+root=insert(root,30)                            
+root=insert(root,40)                            
+root=insert(root,12)                            
+root=insert(root,18)                            
+root=insert(root,25)                            
+root=insert(root,50)                            
+Inorder(root)
+
+Search(root,18)
+Search(root,100)
+delete(root,12)
+print("\n")
+Inorder(root)
