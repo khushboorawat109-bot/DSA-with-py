295aa87200b982517b2cb0e8f9db1572ae010d6b
Khushboo Rawat <khushboorawat109@gmail.com>
2026-03-12 19:58:49 +0530

    graph representation as adjacency list

---
+++ Graph_adjlist.py
@@ -0,0 +1,24 @@
+class Graph:
+    def __init__(self):
+        self.adjlist={}
+    def add_vertex(self,vertex):
+        if vertex not in self.adjlist:
+            self.adjlist[vertex]=[]
+    def addEdge(self,src,dest):
+        self.add_vertex(src)
+        self.add_vertex(dest)
+        self.adjlist[src].append(dest)
+        self.adjlist[dest].append(src)
+    def printGraph(self):
+        for vertex in self.adjlist:
+            print(vertex," -> ",self.adjlist[vertex],end=" \n")
+
+g=Graph()
+g.addEdge(1,2) 
+g.addEdge(2,3) 
+g.addEdge(1,4) 
+g.addEdge(4,3) 
+g.addEdge(2,4) 
+g.addEdge(4,5) 
+g.addEdge(3,5)
+g.printGraph() 
