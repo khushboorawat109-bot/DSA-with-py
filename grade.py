cd438b135ea41bbd0e293c9f68fcb573ead6a711
Khushboo Rawat <khushboorawat109@gmail.com>
2026-05-07 08:31:39 +0530

    grades

---
+++ grade.py
@@ -0,0 +1,18 @@
+# Python program to assign grades to students
+
+marks = int(input("Enter student marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Student Grade:", grade)
