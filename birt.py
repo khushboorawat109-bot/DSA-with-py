d495b07a5e1ecff65aaf96fa2aac00f1f5b03710
Khushboo Rawat <khushboorawat109@gmail.com>
2026-05-09 13:46:32 +0530

    Birthday wishes

---
+++ birt.py
@@ -0,0 +1,8 @@
+name = input("Enter the birthday person's name: ")
+
+print("\n🎂🎉🎈 Happy Birthday! 🎈🎉🎂")
+print(f"Dear {name},")
+print("May your birthday be filled with laughter,")
+print("your year with success,")
+print("and your life with happiness!")
+print("Enjoy your special day! 🥳")
