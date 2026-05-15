4b1820cc30beb735c27b9f90c9b75f8b8c7dac42
Khushboo Rawat <khushboorawat109@gmail.com>
2026-05-16 05:15:06 +0530

    find largest

---
+++ find_large.py
@@ -0,0 +1,18 @@
+# Python program to find the largest among 3 numbers
+
+def find_largest(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+largest = find_largest(num1, num2, num3)
+
+print("Largest number is:", largest)
